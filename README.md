719d13a49a2041ee02ee318fda0c1b48d4261a2f
Ohmsmmm <Ohmsm0150@gmail.com>
2020-03-24 15:07:38 +0700

    add

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# discordAPI
+#SET-UP
+    
+
+#Cr. https://medium.com/@athi.lhong/%E0%B9%80%E0%B8%8A%E0%B9%87%E0%B8%84%E0%B8%8A%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B9%80%E0%B8%82%E0%B9%89%E0%B8%B2%E0%B8%97%E0%B8%B3%E0%B8%87%E0%B8%B2%E0%B8%99-%E0%B8%9A%E0%B8%99-discord-%E0%B8%94%E0%B9%89%E0%B8%A7%E0%B8%A2-node-js-discord-bot-d720c41219d1
---
+++ test.js
@@ -0,0 +1,39 @@
+const discord = require('discord.js');
+const botconfig = require('./botconfig.json')
+const hormBot = new discord.Client({disableEveryone: true});
+const moment = require('moment');
+// const sql = require('mssql');
+// const config = {
+//     user: 'sa',
+//     password: 'P@d0rU123',
+//     server: '167.71.200.91',
+//     database: 'ohmDB'
+// };
+// // connect to your database
+// var err = sql.connect(config)
+// if (err) console.log(err);
+hormBot.on('ready', async () => {
+    console.log(`${hormBot.user.username} is online!!!`);
+});
+hormBot.on("message", async msg => {
+    const username = msg.author.username;
+    if(msg.author.bot) return;
+    if(msg.channel.type === "dm") return;
+
+    let prefix = botconfig.prefix;
+    let msgArray = msg.content.split(" ");
+    let cmd = msgArray[0];
+    let args = msgArray.slice(1);
+    // switch(cmd){
+    //     case `${prefix}hello`: return msg.channel.send(`Hello ${username}`); 
+    //     case `${prefix}big`: return msg.channel.send(`ไอควายบิ๊ก ไอหน้าหี`);
+    //     default: return msg.channel.send(`Command Not Found`);
+    // }
+    if(cmd === `${prefix}hello`){
+        return msg.channel.send(`Hello ${username}`);
+    }
+    if(cmd === `${prefix}big`){
+        return msg.channel.send(`ไอควายบิ๊ก ไอหน้าหี`);
+    }
+});
+hormBot.login(botconfig.token).catch((err) => console.log('err', err));
